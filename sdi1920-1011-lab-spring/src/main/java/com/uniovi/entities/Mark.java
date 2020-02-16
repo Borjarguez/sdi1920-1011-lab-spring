74d9c8a0524c638cbefc137e87e3c26bc0f5b128
Borja <uo258643@uniovi.es>
2020-02-17 00:37:24 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas

--- sdi1920-1011-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1920-1011-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -9,6 +9,10 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	
+	@ManyToOne
+	@JoinColumn(name ="user_id")
+	private User user;
 
 	public Mark() {
 	}
@@ -18,6 +22,13 @@ public class Mark {
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+	
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Long getId() {
@@ -43,6 +54,14 @@ public class Mark {
 	public void setScore(Double score) {
 		this.score = score;
 
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
 	}
 
 	@Override
