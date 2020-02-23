0b15fbde03c77fb001ec4182f5f006e3105cb6a5
Borja <uo258643@uniovi.es>
2020-02-24 00:59:55 +0100

    SDI-SpringBoot-Sesión-4.5-Búsqueda

--- sdi1920-1011-lab-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi1920-1011-lab-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -67,12 +67,25 @@ public class MarksService {
 
 	public List<Mark> getMarksForUser(User user) {
 		List<Mark> marks = new ArrayList<Mark>();
-		
+
 		if (user.getRole().equals("ROLE_STUDENT"))
 			marks = marksRepository.findAllByUser(user);
-		
+
 		if (user.getRole().equals("ROLE_PROFESSOR"))
 			marks = getMarks();
+
+		return marks;
+	}
+
+	public List<Mark> searchMarksByDescriptionAndNameForUser(String searchText, User user) {
+		List<Mark> marks = new ArrayList<Mark>();
+		searchText = "%"+searchText+"%";
+		
+		if (user.getRole().equals("ROLE_STUDENT"))
+			marks = marksRepository.searchByDescriptionNameAndUser(searchText, user);
+		
+		if (user.getRole().equals("ROLE_PROFESSOR"))
+			marks = marksRepository.searchByDescriptionAndName(searchText);
 		
 		return marks;
 	}
